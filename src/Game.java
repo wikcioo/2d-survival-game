a3bf8560517fdd17ad1e3dad0c25c4c8a6c4f4c6
Wiktor <wikcioo@gmail.com>
2021-02-09 15:24:36 +0100

    implemented new thread

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,55 @@
 import display.Display;
 
-public class Game {
+public class Game implements Runnable {
     private Display display;
-
     public int width, height;
+    public String title;
+
+    private Thread thread;
+    boolean running = false;
 
     public Game(String title, int width, int height) {
+        this.title = title;
         this.width = width;
         this.height = height;
+    }
 
+    private void init() {
         display = new Display(title, width, height);
+    }
+
+    @Override
+    public void run() {
+        init();
+
+        while (running) {
+            update();
+            render();
+        }
+
+        stop();
+    }
+
+    private void update() {
+
+    }
+
+    private void render() {
+
+    }
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
